a179b56aa81c4088819ae814539794d233d55201
mario <elplanetadefoxy@gmail.com>
2023-05-08 18:21:12 -0300

    desafio2

--- 2-manejoDeArchivos.js
+++ 2-manejoDeArchivos.js
@@ -29,82 +29,102 @@ Debe tener un método getProductById, el cual debe recibir un id, y tras leer el
 
 
 
+const fs = require ("fs");
+
 class ProductManager {
-    constructor() {
+  constructor(filename) {
+    this.filename = filename;
+    try {
+      const data = readFileSync(this.filename, "utf8");
+      this.products = JSON.parse(data);
+    } catch (error) {
       this.products = [];
     }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    getProductById(id) {
-      const product = this.products.find(p => p.id === id);
-      if (!product) {
-        throw new Error(`El producto con id ${id} no se encuentra`);
-      }
-      return product;
-    }
-  
-    addProduct({ title, description, price, thumbnail, code, stock }) {
-      if (this.products.some(p => p.code === code)) {
-        throw new Error(`Producto con codigo ${code} ya existe`);
-      }
-      const id = this.generateId();
-      const product = { id, title, description, price, thumbnail, code, stock };
-      this.products.push(product);
-      return product;
-    }
-  
-    generateId() {
-      let id;
-      do {
-        id = Math.floor(Math.random() * 1000000);
-      } while (this.products.some(p => p.id === id));
-      return id;
-    }
   }
-  
-  // Creo instancia de ProductManager
-  const manager = new ProductManager();
-  
-  // Obtengo productos (devuelve array vacio)
-  console.log(manager.getProducts());
-  
-  // Agrego productos
-  const product = manager.addProduct({
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-    stock: 25,
-  });
-  console.log(product); // Muestro el objeto de producto con id generado
-  
-  // Obtengo productos (devuelve el array con el producto agregado))
-  console.log(manager.getProducts());
-  
-  // Agrego producto con codigo repetido (muestra error)
-  try {
-    manager.addProduct({
-      title: "otro producto",
-      description: "Este es otro producto",
-      price: 100,
-      thumbnail: "Sin imagen",
-      code: "abc123",
-      stock: 10,
-    });
-  } catch (error) {
-    console.log(error.message);
+
+  saveProducts() {
+    const data = JSON.stringify(this.products);
+    writeFileSync(this.filename, data, "utf8");
   }
-  
-  // Obtengo producto por id (devuelvo el objeto producto)
-  console.log(manager.getProductById(product.id));
-  
-  // Obtengo producto por id inexistente (muestra error)
-  try {
-    manager.getProductById(999999);
-  } catch (error) {
-    console.log(error.message);
+
+  getProducts() {
+    return this.products;
   }
+
+  getProductById(id) {
+    const product = this.products.find(p => p.id === id);
+    if (!product) {
+      throw new Error(`Product with id ${id} not found`);
+    }
+    return product;
+  }
+
+  addProduct({ title, description, price, thumbnail, code, stock }) {
+    if (this.products.some(p => p.code === code)) {
+      throw new Error(`Product with code ${code} already exists`);
+    }
+    const id = this.generateId();
+    const product = { id, title, description, price, thumbnail, code, stock };
+    this.products.push(product);
+    this.saveProducts();
+    return product;
+  }
+
+  updateProduct(id, fields) {
+    const productIndex = this.products.findIndex(p => p.id === id);
+    if (productIndex < 0) {
+      throw new Error(`Product with id ${id} not found`);
+    }
+    const product = { id, ...fields };
+    this.products[productIndex] = product;
+    this.saveProducts();
+    return product;
+  }
+
+  deleteProduct(id) {
+    const productIndex = this.products.findIndex(p => p.id === id);
+    if (productIndex < 0) {
+      throw new Error(`Product with id ${id} not found`);
+    }
+    this.products.splice(productIndex, 1);
+    this.saveProducts();
+  }
+
+  generateId() {
+    let id = 1;
+    if (this.products.length > 0) {
+      id = this.products[this.products.length - 1].id + 1;
+    }
+    return id;
+  }
+}
+
+// Creo instancia de ProductManager
+const manager = new ProductManager("products.json");
+
+// Obtengo productos (devuelve un array con los productos)
+console.log(manager.getProducts());
+
+// Agrego producto
+const product = manager.addProduct({
+  title: "producto prueba",
+  description: "Este es un producto prueba",
+  price: 200,
+  thumbnail: "Sin imagen",
+  code: "abc123",
+  stock: 25,
+});
+console.log(product); //Muestro el objeto producto con id generado
+
+// Obtengo productos (devuelve el array con el producto agregado)
+console.log(manager.getProducts());
+
+// Obtengo producto por id (devuelve el objeto producto)
+console.log(manager.getProductById(product.id));
+
+// Actualizo producto
+const updatedProduct = manager.updateProduct(product.id, { price: 300, stock: 10 });
+console.log(updatedProduct); // Muestro el objeto producto actualizado
+
+// Elimino producto
+manager.deleteProduct(product.id);
